cff00353bc58c1ef8e22783cf31bf183d05d09fc
Luca Tamburrano <ltamburrano@gmail.com>
2018-08-08 17:53:45 +0200

    Add files via upload

--- NeuroPy.py
+++ NeuroPy.py
@@ -26,6 +26,8 @@
 # NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
+# Enhancements from the original version by xcottos (Luca Tamburrano)
+
 import serial
 import time
 import sys
